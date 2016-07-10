fd45b33ffc3180b58b68e5b6a5497944cd3fec73
ashishnembang <ashish.nembang@gmail.com>
2016-07-10 18:16:36 -0500

    aricaft class
    annotation setter getter

---
+++ src/main/java/com/cs544/ftrms/model/Aircaft.java
@@ -0,0 +1,86 @@
+package com.cs544.ftrms.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+public class Aircaft {
+	@Id
+	@GeneratedValue
+	private int id;
+	private long aircraftNumber;
+	private String callSign;
+	private String aircraftType;
+	
+	@Temporal(TemporalType.DATE)
+	private Date airworthyDate;
+	
+	@Temporal(TemporalType.DATE)
+	private Date nextInspectionDate;
+
+
+	public Aircaft(){}
+	
+	public Aircaft(long aircraftNumber, String callSign, String aircraftType, Date airworthyDate,
+			Date nextInspectionDate) {
+		super();
+		this.aircraftNumber = aircraftNumber;
+		this.callSign = callSign;
+		this.aircraftType = aircraftType;
+		this.airworthyDate = airworthyDate;
+		this.nextInspectionDate = nextInspectionDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public long getAircraftNumber() {
+		return aircraftNumber;
+	}
+
+	public void setAircraftNumber(long aircraftNumber) {
+		this.aircraftNumber = aircraftNumber;
+	}
+
+	public String getCallSign() {
+		return callSign;
+	}
+
+	public void setCallSign(String callSign) {
+		this.callSign = callSign;
+	}
+
+	public String getAircraftType() {
+		return aircraftType;
+	}
+
+	public void setAircraftType(String aircraftType) {
+		this.aircraftType = aircraftType;
+	}
+
+	public Date getAirworthyDate() {
+		return airworthyDate;
+	}
+
+	public void setAirworthyDate(Date airworthyDate) {
+		this.airworthyDate = airworthyDate;
+	}
+
+	public Date getNextInspectionDate() {
+		return nextInspectionDate;
+	}
+
+	public void setNextInspectionDate(Date nextInspectionDate) {
+		this.nextInspectionDate = nextInspectionDate;
+	}
+
+	
+}
