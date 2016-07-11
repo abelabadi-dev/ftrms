94d472087cf25aa544e0df347cbbf2f20e6421b1
ashishnembang <ashish.nembang@gmail.com>
2016-07-11 00:07:22 -0500

    changes

--- src/main/java/com/cs544/ftrms/controller/MaintainanceEnginnerController.java
+++ src/main/java/com/cs544/ftrms/controller/MaintainanceEnginnerController.java
@@ -1,8 +1,14 @@
 package com.cs544.ftrms.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+
 import com.cs544.ftrms.model.IAircraft;
 
+@Controller
 public class MaintainanceEnginnerController {
+	@Resource
 	private IAircraft aircraft;
 
 }
