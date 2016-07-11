73d126e5de46ea848997741d41d2e84880aacef2
ashishnembang <ashish.nembang@gmail.com>
2016-07-10 22:25:29 -0500

    updated aircraft dao and service

--- src/main/java/com/cs544/ftrms/dao/AircraftDao.java
+++ src/main/java/com/cs544/ftrms/dao/AircraftDao.java
@@ -27,6 +27,11 @@ public class AircraftDao {
 		return (Aircraft)sessionFactory.getCurrentSession().get(Aircraft.class, id);
 	}
 	
+	public void deleteAircraft(int id){
+		Aircraft ac = (Aircraft)sessionFactory.getCurrentSession().load(Aircraft.class, id);
+		sessionFactory.getCurrentSession().delete(ac);
+	}
+	
 	@SuppressWarnings("unchecked")
 	public List<Aircraft> getAllAircrafts(){
 		return sessionFactory.getCurrentSession().createQuery("from Aircraft").list();
--- src/main/java/com/cs544/ftrms/service/AircraftService.java
+++ src/main/java/com/cs544/ftrms/service/AircraftService.java
@@ -1,8 +1,43 @@
 package com.cs544.ftrms.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cs544.ftrms.dao.AircraftDao;
+import com.cs544.ftrms.model.Aircraft;
 
 @Service
 public class AircraftService {
+	@Autowired
+	SessionFactory sessionFactory;
+	private AircraftDao aircraftDao;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	public void setAircraftDao(AircraftDao aircraftDao) {
+		this.aircraftDao = aircraftDao;
+	}
+	
+	@Transactional
+	public List<Aircraft> getListOfAircraft(){
+		return aircraftDao.getAllAircrafts();
+		
+	}
+	
+	public Aircraft getAircraft(int id){
+		return aircraftDao.getAircraft(id);
+	}
+	
+	public void deleteAircraft(int id){
+		aircraftDao.deleteAircraft(id);
+	}
+	
+	
 	
 }
