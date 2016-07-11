c3dc3f20dd0d8e41f4327045757350f7ce59507e
ashishnembang <ashish.nembang@gmail.com>
2016-07-10 21:57:28 -0500

    aircraft dao...updated

---
+++ src/main/java/com/cs544/ftrms/dao/AircraftDao.java
@@ -0,0 +1,34 @@
+package com.cs544.ftrms.dao;
+
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.cs544.ftrms.model.Aircraft;
+
+@Repository
+public class AircraftDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public AircraftDao(SessionFactory sessionFactory){
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public void addAircraft(Aircraft aircraft){
+		sessionFactory.getCurrentSession().save(aircraft);	
+	}
+	
+	public Aircraft getAircraft(int id){
+		return (Aircraft)sessionFactory.getCurrentSession().get(Aircraft.class, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Aircraft> getAllAircrafts(){
+		return sessionFactory.getCurrentSession().createQuery("from Aircraft").list();
+	}
+}
---
+++ src/main/java/com/cs544/ftrms/service/AircraftService.java
@@ -0,0 +1,8 @@
+package com.cs544.ftrms.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AircraftService {
+	
+}
